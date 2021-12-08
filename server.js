f4813ea6277d8eeb928235fd382c5d4a6c866505
Opilane TTHK <opilane@tthk.ee>
2021-12-08 14:42:45 +0200

    WishList Update

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const date = require(__dirname+'/getDate.js'); 
 
 const app = express();
 
 app.get('/', (request, response) => {
     response.send('Hello, World!')
-})
+});
 
+app.get('/getdate', (req, res) => { //req - request, res - response
+    let today = date.getDate();
+    res.send(today);
 
+});
 
+app.get('/weekday', (req, res) => {
+    let weekday = date.getWeekDay();
+    res.send(weekday)
+});
 
+app.get('*', (req, res) => {
+    res.send('404. page not found')
+});
 
 port = 3000;
 app.listen(port, () => {
