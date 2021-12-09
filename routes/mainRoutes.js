15447351d6f9630304aa78e7babfdb916e38342e
Opilane TTHK <opilane@tthk.ee>
2021-12-09 13:45:58 +0200

    WishList Finished

---
+++ routes/mainRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const date = require('../getDate.js')
+const mainController = require('../controllers/mainController')
+const router = express.Router();
+
+router.get('/', mainController.getMainPage);
+
+router.get('/getdate', mainController.getDate);
+
+router.get('/weekday', mainController.getWeekday);
+
+router.post('/', mainController.postWish);
+
+module.exports = router;
